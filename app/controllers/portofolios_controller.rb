44e32175d22a1472b3abc8b46577e2854354e9c4
hamzeahmed <hamzeahmedx@hotmail.com>
2021-03-14 13:52:16 +0300

    edit and updated is created

--- app/controllers/portofolios_controller.rb
+++ app/controllers/portofolios_controller.rb
@@ -8,7 +8,7 @@ class PortofoliosController < ApplicationController
   end
 
   def create
-    @portofolio_item = Portofolio.new(params.require(:portofolio).permit(:title,:sub_title, :body))
+    @portofolio_item = Portofolio.new(params.require(:portofolio).permit(:titile,:sub_title, :body))
 
     respond_to do |format|
       if @portofolio_item.save
@@ -17,6 +17,24 @@ class PortofoliosController < ApplicationController
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @portofolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+    @portofolio_item = Portofolio.find(params[:id])
+  end
+
+  def update
+    @portofolio_item = Portofolio.find(params[:id])
+
+    respond_to do |format|
+      if @portofolio_item.update(params.require(:portofolio).permit(:titile,:sub_title, :body))
+        format.html { redirect_to portofolios_path, notice: "Portofolio was successfully updated." }
+        format.json { render :show, status: :ok, location: @blog }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
