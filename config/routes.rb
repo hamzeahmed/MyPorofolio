46a0561ca2bb90f1f74d1678cee6e867dfe657f5
hamzeahmed <hamzeahmedx@hotmail.com>
2021-03-15 18:09:00 +0300

    added srft and status for custom action qand routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get 'portofolio/:id', to: 'portofolios#show', as: 'portofolio_show'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
   root 'pages#home'
 end
