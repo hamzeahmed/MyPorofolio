1756eff476d7f508c784ab07e4d04db53f6edad0
hamzeahmed <hamzeahmedx@hotmail.com>
2021-04-01 18:11:12 +0300

    implemented devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portofolios, except: [:show]
   get 'portofolio/:id', to: 'portofolios#show', as: 'portofolio_show'
   get 'about-me', to: 'pages#about'
