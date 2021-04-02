3cc948e389fed57ec0152fb91cdb5c2ec5b8b0ce
hamzeahmed <hamzeahmedx@hotmail.com>
2021-04-02 20:44:24 +0300

    name is created for registation and update in User Model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :condigure_permited_params,  if: :devise_controller?
+
+  def condigure_permited_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
 end
