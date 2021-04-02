a952e5f984d854d2e1ffec7a7ffa78789dee8fec
hamzeahmed <hamzeahmedx@hotmail.com>
2021-04-02 20:59:01 +0300

    implemented controller concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :condigure_permited_params,  if: :devise_controller?
-
-  def condigure_permited_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  end
-
+include DeviseWhitelist
 end
