0cc602d81ad35990200ccd2a145f129093cfe3da
hamzeahmed <hamzeahmedx@hotmail.com>
2021-04-07 15:02:48 +0300

    copy rights implememnted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   include DeviseWhitelist
   include CurrentUserConcern
   include SetPageDefaultContents
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = HamzeViewTool::Renderer.copyright 'Hamze Ahmed', 'All right reserved'
+  end
+end
+
+module HamzeViewTool
+  class Renderer
+    def self.copyright name,msg
+      "&copy; #{Time.now.year} | #{name} | #{msg}".html_safe
+    end
+  end
 end
